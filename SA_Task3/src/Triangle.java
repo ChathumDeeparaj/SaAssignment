397be2634be49d78454fce6c9cbc85c026f19ce2
ChathumDeeparaj <164840623+ChathumDeeparaj@users.noreply.github.com>
2024-08-23 11:08:08 +0530

    Add files via upload

---
+++ SA_Task3/src/Triangle.java
@@ -0,0 +1,9 @@
+public class Triangle implements Shape {
+    public void draw(){
+        System.out.println("    *   ");
+        System.out.println("   ***    ");
+        System.out.println("  *****   ");
+        System.out.println(" *******  ");
+        System.out.println("********* ");
+    }
+}
